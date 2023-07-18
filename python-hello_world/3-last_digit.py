b527ab2519903a9a11426757ce1ac9adac2f6a61
chykB <malikchika86@gmail.com>
2023-07-18 15:03:22 -0700

    print random number

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -6,7 +6,7 @@ if last_digit  > 5:
     print(f"Last digit of {number} is {last_digit} and is greater than 5")
 elif last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
-else:
+elif last_digit != 0 and < 6:
     print(f"Last digit of {number} is -{last_digit} and is less than 6 and not 0")
 
 
