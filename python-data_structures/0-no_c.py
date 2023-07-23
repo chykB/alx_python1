0e2a8cb91e8bb9325a47b566fb5538e084bf00e3
chykB <malikchika86@gmail.com>
2023-07-23 13:08:08 -0700

    python data structures

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -3,6 +3,5 @@ def no_c(my_string):
     n_string = ""
     for i in my_string:
         if i != 'c' and i != 'C':
-            n_string += i
-       
+            n_string += i    
     return n_string
