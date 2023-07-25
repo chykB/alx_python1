3c4a273610e4ca87c43d95ae0d099a0d65e281cb
chykB <malikchika86@gmail.com>
2023-07-25 03:35:35 -0700

    update

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -4,5 +4,5 @@ def is_prime(number):
         return False
     for i in range(2, int(number**0.5)+1):
         if (number % i) == 0:
-            return True
-    return False
+            return False
+    return True
