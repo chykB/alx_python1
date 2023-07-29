ac694984ef91955e4e74cd206d62175c5e71d313
chykB <malikchika86@gmail.com>
2023-07-29 15:53:56 -0700

    Python classes

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-#Create a class module
+
+
 class Square:
     """Represents a square
     Attributes:
-        __size (int): size of a side of the square
-    """
+        __size: the size of square
+        """
     def __init__(self, size):
         """Initializes a square
         Args:
-            size (int): size of a side of the square
+            size: size of a side of the square
         Returns: None
         """
         self.__size = size
