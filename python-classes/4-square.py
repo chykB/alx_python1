1fadf6f0e128e82f8b320ca87c512b89362f8f74
chykB <malikchika86@gmail.com>
2023-07-31 13:51:14 -0700

    python classes

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -7,15 +7,22 @@ class Square:
     Attributes:
         __size (int): size of a side of the square
     """
-
     def __init__(self, size=0):
         """initializes the square
-            Args:
-                __size (int): size of a size of the square
-            Returns:
-                None
+        Args:
+            size (int): size of a side of the square
+        Returns:
+            None
         """
-        self.__size = size
+        self.size = size
+
+    def area(self):
+        """calculates the square's area
+        Returns:
+            The area of the square
+        """
+        return (self.__size) ** 2
+
     @property
     def size(self):
         """getter of __size
@@ -23,27 +30,23 @@ class Square:
             The size of the square
         """
         return self.__size
+
     @size.setter
     def size(self, value):
-         """setter of __size
+        """setter of __size
         Args:
-            value (int): the size of a size of the square
+            value (int): size of a side of the square
         Returns:
             None
         """
         if type(value) is not int:
             raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
         else:
-            self.__size = value
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
 
-    def area(self):
-        """calculates the square's area
-        Returns:
-            The area of the square
-        """
-        return self.__size ** 2
     def my_print(self):
         """prints the square
         Returns:
