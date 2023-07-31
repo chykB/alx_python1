6eaf7a6b629af77d752e150413a3ebdf5b00f8ed
chykB <malikchika86@gmail.com>
2023-07-31 16:51:03 -0700

    python inheritance

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+
+
+"""the function prototype"""
 def is_kind_of_class(obj, a_class):
+    """
+        obj: the object
+        a_class: the class to be checked against
+    """
     return isinstance(obj, a_class)
