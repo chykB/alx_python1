912ce955d88d36833d7b7a39d0fc52c3cb3e642c
chykB <malikchika86@gmail.com>
2023-07-31 16:29:45 -0700

    python inheritance

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -3,6 +3,10 @@
 
 """the function prototype"""
 def is_same_class(obj, a_class):
+    """
+        obj: the object
+        a_class: the class
+    """
     if type(obj) is a_class:
         return True
     else:
