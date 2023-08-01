2b28653f6f782e52a939091a5e371eecbe802e1c
chykB <malikchika86@gmail.com>
2023-08-01 15:49:32 -0700

    create a class module and raise an exception

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,11 +1,37 @@
 #!/usr/bin/python3
-from 5-base_geometry import BaseGeometry
+"""a base moduleclass"""
+
+
+class BaseGeometry:
+    """a method that excludes the __init_subclass__ from the dir"""
+    def __dir__(cls) -> None:
+        attributes = super().__dir__()
+        return [attribute for attribute in attributes if attribute != "__init_subclass__"]
+
+    """a method that calculates the area but is not implemented"""
+    def area(self):
+        """an exception raised"""
+        raise Exception("area() is not implemented")
+
+
+    def integer_validator(self, name, value):
+        self.name = "name"
+        """
+        value: validating the value
+        """
+        if type(value) is int:
+            if value <= 0:
+                raise ValueError("{} must be greater than 0".format(name))
+            else:
+                self.value = value
+        else:
+            raise TypeError("{} must be an integer".format(name))
 
 """a subclass Rectangle"""
 class Rectangle(BaseGeometry):
     """excluding the dir subclass from the dir list"""
     def __dir__(cls) -> None:
-        attributes = super().__dir__()
+        attributes = Rectangle().__dir__()
         return [attribute for attribute in attributes if attribute != "__init_subclass__"]
 
 
