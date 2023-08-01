d01523cf6827bd5bb48c8d316f73f8a51abab038
chykB <malikchika86@gmail.com>
2023-08-01 12:21:24 -0700

    create a class module

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -4,3 +4,9 @@
 
 class BaseGeometry:
     """this is an empty class"""
+    pass
+    
+    def __dir__(cls) -> None:
+        """listt all attribute of this class exclude the __init__subclass"""
+        attributes = super().__dir__()
+        return [attribute for attribute is attributes if attribute != "__init__subclass"]
