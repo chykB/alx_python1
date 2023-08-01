d1946c27bb6eb0f9694a41ef9cd663ce6fa70e90
chykB <malikchika86@gmail.com>
2023-08-01 13:03:50 -0700

    create a class module and raise an exception

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -5,6 +5,17 @@
 class BaseGeometry:
     """a method that the area but it is not implemented"""
 
+
+    def __dir__(cls) -> None:
+        """list all attribute except the __init_subclass__"""
+        attributes = super().__dir__()
+        dir_to_return = []
+        for attribute in attributes:
+            if attribute != "__init_subclass__":
+                dir_to_return.append(attribute)
+        return dir_to_return
+
+
     def area(self):
         """this method raises an exception"""
         raise Exception("area() is not implemented")
