c0c254527e4137e57b401c708e66b7f977b8bc0f
chykB <malikchika86@gmail.com>
2023-08-01 12:42:43 -0700

    create a class module

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a class module BaseGeometry"""
+
+
+class BaseGeometry:
+    """a method that the area but it is not implemented"""
+
+    def area(self):
+        """this method raises an exception"""
+        raise Exception("area() is not implemented")
