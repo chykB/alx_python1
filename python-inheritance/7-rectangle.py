16199585d1dd086f2feb5c6889e7741f45f56637
chykB <malikchika86@gmail.com>
2023-08-01 17:15:48 -0700

    create a class module and raise an exception

---
+++ python-inheritance/7-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""a base moduleclass"""
+
+
+class BaseGeometry:
+
+    """a method that calculates the area but is not implemented"""
+    def area(self):
+        """an exception raised"""
+        raise Exception("area() is not implemented")
+
+
+    def integer_validator(self, name, value):
+        self.name = "name"
+        """
+        value: validating the value
+        """
+        if type(value) is int:
+            if value <= 0:
+                raise ValueError("{} must be greater than 0".format(name))
+            else:
+                self.value = value
+        else:
+            raise TypeError("{} must be an integer".format(name))
+
+"""a subclass Rectangle"""
+class Rectangle(BaseGeometry):
+
+    """a method that excludes the __init_subclass__ from the dir"""
+    def __dir__(self) -> None:
+        attributes = dir(self).__dir__()
+        return [attribute for attribute in attributes if attribute != "__init_subclass__"]
+
+
+    """Instantiation with width and height"""
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    """the area of the rectangle implemented"""
+    def area(self):
+        return self.__width * self.__height
+
+    """print out rectangle description"""
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
