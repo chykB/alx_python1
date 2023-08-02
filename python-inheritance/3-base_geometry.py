5e71429ab0fbda0bf30f46e6ce48eb30bfe25318
chykB <malikchika86@gmail.com>
2023-08-02 14:52:24 -0700

    update

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+"""a class module BaseGeometry"""
 
-"""an empty class BaseGeometry"""
+
 class BaseGeometry:
-    """this is an empty class"""    
-     def __dir__(cls) -> None:
-        """
-           list all attribute except the __init_subclass__
-        """
+    """a method that the area but it is not implemented"""
+
+
+    def __dir__(cls) -> None:
+        """list all attribute except the __init_subclass__"""
         attributes = super().__dir__()
-        return [attribute for attribute in attributes if attribute != "__init_subclass__"]
+        dir_to_return = []
+        for attribute in attributes:
+            if attribute != "__init_subclass__":
+                dir_to_return.append(attribute)
+        return dir_to_return
