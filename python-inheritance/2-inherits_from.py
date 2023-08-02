9c20c6127a66f8d8e276dfdbcdccf45d5f97c427
chykB <malikchika86@gmail.com>
2023-08-02 14:18:36 -0700

    return a subclass

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -2,7 +2,7 @@
 
 
 
-"""return if the object is an instance of a class that inherited from a class"""
+"""return if the object is an instance of a class that"""
 def inherits_from(obj, a_class):
     """the return statement"""
     return isinstance(obj, a_class) and type(obj) != a_class
