b053bcf3784a6fb908e9d301345fb1e7f6d8c735
chykB <malikchika86@gmail.com>
2023-08-05 16:41:55 -0700

    python class

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""this module contains the base class its objects and methods """
+
+
+class Base:
+    """this base class will serve as a super class to 
+    other classes in this module"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        init__    the class constructor.
+        """
+        if id is not None:
+            self.id = id
+        else:
+           self. __nb_objects += 1
+            self.id = self.__nb_objects
