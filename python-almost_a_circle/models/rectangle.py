14e3623e7d5f1a09518e9f8f1ccb03c2cf4232eb
chykB <malikchika86@gmail.com>
2023-08-08 03:25:37 -0700

    addin a __str__ method to convert a Rectangle object to a string

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -113,4 +113,4 @@ class Rectangle(Base):
 
     def __str__(self):
         """this method convert object to string"""
-        return str("[Rectangle] {} {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height))
+        return str("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height))
