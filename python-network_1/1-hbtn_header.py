2117cc5cf52311d38aa2ee05e1a947c8b52ef129
chykB <malikchika86@gmail.com>
2023-08-09 07:34:03 -0700

    takes in a URL, sends a request to the URL and displays the value of the variable X-Request-Id in the response header

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""importing module and package"""
+import requests
+import sys
+
+def get_x_request_id(url):
+    """Gets the value of the X-Request-Id header in the response to a request to the specified URL"""
+    response = requests.get('https://alu-intranet.hbtn.io/status')
+    return response.header['X-Request-Id']
+if__name__=='__main__':
+    url = sys.argv[1]
+    print(get_x_request_id(url))
