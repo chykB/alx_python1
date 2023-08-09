7fc13edadfbee08d8fe012b0c59a97bb63caaec2
chykB <malikchika86@gmail.com>
2023-08-08 18:48:00 -0700

    http request

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,7 +2,7 @@
 """this module is use to make a request"""
 import requests
 if __name__=="__main__":
-    response = requests.get('https://alu-intranet.hbtn.io/status')
+    response = requests.get('http://0.0.0.0:5050/status')
     if response.status_code == 200:
         print("Body response:")
         print("\t- type:", type(response.text))
