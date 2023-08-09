881f003ef3d19296e3472cfc7f7c0efda97bdf63
chykB <malikchika86@gmail.com>
2023-08-09 11:44:04 -0700

    update

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-
 """importing requests module and sys packages"""
 import requests
 import sys
-
-
 def get_x_request_id(url):
     """Gets the value of the X-Request-Id header in the response to a request to the specified URL"""
     response = requests.get('https://alu-intranet.hbtn.io/status')
     return response.header['X-Request-Id']
-
-
 if__name__=='__main__':
     """execute the script when it is run as a standalone program."""
     url = sys.argv[1]
