e97ec1c9635eee9297994025ee9cc896f2e963d2
chykB <malikchika86@gmail.com>
2023-08-08 18:44:17 -0700

    http request

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,6 +3,9 @@
 import requests
 if __name__=="__main__":
     response = requests.get('https://alu-intranet.hbtn.io/status')
-    print("Body response:")
-    print("\t- type:", type(response.text))
-    print("\t- content:", response.text)
+    if response.status_code == 200:
+        print("Body response:")
+        print("\t- type:", type(response.text))
+        print("\t- content:", response.text)
+    else:
+        print(response.status_code)
