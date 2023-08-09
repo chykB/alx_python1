481997f618823187cd4461748f110e85c6566e6f
chykB <malikchika86@gmail.com>
2023-08-08 18:35:54 -0700

    http request

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """this module is use to make a request"""
 import requests
-
-req = requests.get('https://alu-intranet.hbtn.io/status')
+if __name__=="__main__":
+    response = requests.get('https://alu-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
