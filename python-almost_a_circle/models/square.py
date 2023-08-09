c241f8fd1218cb0e633228b775666cd587cfba67
chykB <malikchika86@gmail.com>
2023-08-08 18:08:43 -0700

    Write the class Square that inherits from Rectangle

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -21,9 +21,9 @@ class Square(Rectangle):
     def size(self, value):
         """The setter method for y"""
         if type(value) != int:
-            raise TypeError("size must be an integer")
+            raise TypeError("width must be an integer")
         if value <= 0:
-            raise ValueError("size must be > 0")
+            raise ValueError("width must be > 0")
         self.width = value
         self.height = value
 
