863eb9bb6fe006817b1fc322e78fb7e0e3608654
chykB <malikchika86@gmail.com>
2023-08-10 10:39:34 -0700

    a Python script that takes in a URL, sends a request to the URL and displays the body of the response

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
-"""import request and sys"""
+"""Sends a GET request to the specified URL and prints the body of the response.
+
+If the HTTP status code is greater than or equal to 400, print: Error code: followed by the value of the HTTP status code
+"""
+
 import requests
 import sys
+
 def main():
+  """Sends a GET request to the specified URL and prints the body of the response."""
   url = sys.argv[1]
+
   res = requests.get(url)
 
-  print(res.text)
   if res.status_code >= 400:
-    print("Error code: {} ".format(res.status_code))
+    print('Error code: {}'.format(res.status_code))
+  else:
+    print(res.text)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
   main()
