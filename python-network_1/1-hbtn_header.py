4f7a2297542882107ade02c3304b005359d6eede
chykB <malikchika86@gmail.com>
2023-08-10 08:44:51 -0700

    update

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -2,15 +2,20 @@
 """importing requests module and sys packag"""
 import requests
 import sys
-def get_x_request_id(url, x_request_id):
-  """Gets the value of the X-Request-Id header in the response to a request to the specified URL."""
-  headers = {'X-Request-Id':x_request_id}
-  response = requests.get(url, headers=headers)
-  return response.headers['X-Request-Id']
-if __name__== "__main__":
-    if len(sys.argv) < 3:
-        sys.exit(1)
+def get_request_id(url):
+    try:
+        res = requests.get(url)
+        x_request_id = res.headers.get('X-Request-Id')
+        if x_request_id:
+            print(x_request_id)
+        else:
+            print("Request id not found")
+    except requests.exceptions.RequestException as e:
+        print("Error: {}".format(e)
+        exit()
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <url>")
+        exit()
     url = sys.argv[1]
-    x_request_id = sys.argv[2]
-    result = get_x_request_id(url, x_request_id)
-    print(result)
+    get_request_id(url)
