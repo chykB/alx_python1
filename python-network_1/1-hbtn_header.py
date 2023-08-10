ce83c2480a8e9604cc18d801703a83e9874cb20a
chykB <malikchika86@gmail.com>
2023-08-10 09:11:46 -0700

    update

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,21 +1,36 @@
 #!/usr/bin/python3
-"""importing requests module and sys packag"""
+"""Gets the X-Request-Id header from a URL."""
+
 import requests
 import sys
+
 def get_request_id(url):
-    try:
-        res = requests.get(url)
-        x_request_id = res.headers.get('X-Request-Id')
-        if x_request_id:
-            print(x_request_id)
-        else:
-            print("Request id not found")
-    except requests.exceptions.RequestException as e:
-        print("Error: {}".format(e)
-        sys.exit(1)
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <url>")
-        sys.exit(1)
-    url = sys.argv[1]
-    get_request_id(url)
+  """Gets the X-Request-Id header from a URL.
+
+  Args:
+    url: The URL to get the request id from.
+
+  Returns:
+    The X-Request-Id header, or None if the header is not found.
+  """
+
+  try:
+    res = requests.get(url)
+    x_request_id = res.headers.get('X-Request-Id')
+  except requests.exceptions.RequestException as e:
+    print('Error: {}'.format(e))
+    sys.exit(1)  # <-- This line is indented now.
+
+  return x_request_id
+
+if __name__ == '__main__':
+  if len(sys.argv) != 2:
+    print('Usage: python script.py <url>')
+    sys.exit(1)
+
+  url = sys.argv[1]
+  x_request_id = get_request_id(url)
+  if x_request_id:
+    print(x_request_id)
+  else:
+    print('Request id not found')
