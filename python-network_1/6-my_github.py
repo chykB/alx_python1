819a55db9cc2aba977a53d59f83695aac4546f9a
chykB <malikchika86@gmail.com>
2023-08-10 19:02:11 -0700

    this takes a username and password and display github id

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -2,25 +2,22 @@
 """this program uses the GitHub API to display my id"""
 import requests
 import sys
-
-def github_d(username, password):
-    url = 'https://api.github.com/users/{}'.format(username, password)
+def get_github_id(username, password):
+    """Gets the GitHub id for the specified username and password."""
+    url = 'https://api.github.com/users/{}'.format(username)
     auth = requests.auth.HTTPBasicAuth(username, password)
     response = requests.get(url, auth=auth)
-    try:
-        if response.status_code == 200:
-            return response.json()['id']
-        elif response.status_code == 401:
-            print("None")
+    if response.status_code == 200:
+        return response.json()['id']
+    else:
+        if response.status_code == 401:
+            print('None')
         else:
-            print("Error: {}".format(response.status_code))
+            print('Error getting GitHub id: {}'.format(response.status_code))
             sys.exit(1)
-    except requests.exceptions.HTTPError as e:
-        print('Error getting GitHub id: {}'.format(e))
-        sys.exit(1)
 
 if __name__ == "__main__":
-    username =  sys.argv[1]
+    username = sys.argv[1]
     password = sys.argv[2]
-    id = github_d(username, password)
-    print("{}".format(id)
+    id = get_github_id(username, password)
+    print('{}'.format(id))
