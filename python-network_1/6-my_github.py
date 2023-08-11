e93584b11ddba96ec35f221006b5ac976d12c035
chykB <malikchika86@gmail.com>
2023-08-10 18:38:52 -0700

    this takes a username and password and display github id

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -7,14 +7,20 @@ def github_d(username, password):
     url = 'https://api.github.com/users/{}'.format(username, password)
     auth = requests.auth.HTTPBasicAuth(username, password)
     response = requests.get(url, auth=auth)
-    if response.status_code == 200:
-        return response.json()['id']
-    else:
-        print("Error: {}".format(response.status_code))
+    try:
+        if response.status_code == 200:
+            return response.json()['id']
+        elif response.status_code == 401:
+            print("None")
+        else:
+            print("Error: {}".format(response.status_code))
+            sys.exit(1)
+    except requests.exceptions.HTTPError as e:
+        print('Error getting GitHub id: {}'.format(e))
         sys.exit(1)
 
 if __name__ == "__main__":
     username =  sys.argv[1]
     password = sys.argv[2]
     id = github_d(username, password)
-    print("{}".format(id))
+    print("{}".format(id)
