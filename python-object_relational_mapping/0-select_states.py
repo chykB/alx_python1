29af3a688276182ceb1bd054746b4f46521fe36b
chykB <malikchika86@gmail.com>
2023-08-20 14:24:23 -0700

    connect to mySQL and list all state

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,8 +4,9 @@ list all state from the database hbtn_0e_0_usa, it takes 3 argument"""
 import MySQLdb
 import sys
 connect = MySQLdb.connect(
-        host='localhost', user=sys.argv[1], password=sys.argv[2], database=sys.argv[3], port=3306
-        )
+        host='localhost', user=sys.argv[1], password=sys.argv[2],
+        database=sys.argv[3], port=3306
+)
 cursor = connect.cursor()
 query = "SELECT * FROM states ORDER BY id ASC"
 cursor.execute(query)
