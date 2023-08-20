cfcfdcfa5db78d6cff645f6d47524d8f389fc9ad
chykB <malikchika86@gmail.com>
2023-08-20 15:27:02 -0700

    connect to mySQL and list all state where name matches the the searched name

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+connect = MySQLdb.connect(
+        host="localhost", user=sys.argv[1], password=sys.argv[2],
+        database=sys.argv[3], name=sys.argv[4], port=3306
+)
+input_name = input("Enter a state name: ")
+cursor = connect.cursor()
+query = "SELECT * FROM states WHERE name LIKE '{}%' ORDER BY id ASC".format(input_name)
+cursor.execute(query)
+for obj in cursor:
+    print(obj)
+cursor.close()
+connect.close()
