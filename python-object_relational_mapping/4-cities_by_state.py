763fdbd2034fbde5360ffd40a007e44159b971ff
chykB <malikchika86@gmail.com>
2023-08-20 16:56:20 -0700

    connect to mySQL and list all cities of the state in the database

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -2,8 +2,8 @@
 import MySQLdb
 import sys
 connect = MySQLdb.connect(
-        host="localhost", user=sys.argv[1], password=sys.argv[2]
-        database=sys.argv[3]
+        host="localhost", user=sys.argv[1], password=sys.argv[2],
+        database=sys.argv[3], port=3306
 )
 state_name = sys.argv[4]
 cursor = connect.cursor()
