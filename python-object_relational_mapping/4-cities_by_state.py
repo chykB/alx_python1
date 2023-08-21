9c98b2461ceb0b1707afd232eda3f261549208ec
chykB <malikchika86@gmail.com>
2023-08-20 17:21:43 -0700

    connect to mySQL and list all cities of the state in the database

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,7 @@ cursor = connect.cursor()
 cursor.execute(
         """SELECT cities.id, cities.name, states.name
         FROM cities
-        INNER JOIN states ON cities.state_id=state.id
+        INNER JOIN states ON cities.state_id=states.id
         ORDER BY cities.id ASC;
         """
 )
