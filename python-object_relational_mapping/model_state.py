186eb566576e19e1a8bf7e1a041d6c2c746863d0
chykB <malikchika86@gmail.com>
2023-08-24 08:15:33 -0700

    SQLAlchemy - the class definition of a State and an instance

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 """create a class definition of a State and an instance Base = declarative_base()"""
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column
-from models import Base, State
-Base = declarative_base()
+from sqlalchemy import Column, Integer, String, MetaData
+meta_data = MetaData()
+Base = declarative_base(MetaData=meta_data)
 class State(Base):
     __tablename__ = states
-    id = Column(Integer, primary_key=True, auto_increment=True, nullable=False)
+    id = Column(Integer, primary_key=True, unique=True, auto_increment=True, nullable=False)
     name = Column(String(128), nullable=False)
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}@localhost/{}port=3306'.format(user, password) 
-    Base.metadata.create_all(engine)
