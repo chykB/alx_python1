41f3811381f2dcfd7859f7edf675f8105c53b929
chykB <malikchika86@gmail.com>
2023-08-28 12:24:19 -0700

    a script that starts a Flask web application

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+"""this script start the Flask web application"""
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    """the route to home page"""
     return "<p>Hello HBNB!<p>"
 
 if __name__ == "__main__":
