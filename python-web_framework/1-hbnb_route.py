ec3762ae297e99e9b226ddccb3d1ed607dd32d9f
chykB <malikchika86@gmail.com>
2023-08-28 17:50:58 -0700

    a script tha start web application with /hbnb route

---
+++ python-web_framework/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module contains the code for a simple Flask web application.
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    """
+    The application has a single route, `/`,
+    which calls the `index()` function
+    """
+    return "Hello HBNB!"
+
+@app.route("/hbnb")
+def hbnb():
+    return "HBNB"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
