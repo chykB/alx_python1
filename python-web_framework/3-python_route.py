8448ef5913787a41231ac479ef7c8c40b47bc346
chykB <malikchika86@gmail.com>
2023-08-28 18:21:55 -0700

    a script that defines  a dynamic route that display a text with a default value.

---
+++ python-web_framework/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+This module contains the code for a simple Flask web application.
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    """
+    The application has a single route, `/`,
+    which calls the `index()` function
+    """
+    return "Hello HBNB!"
+
+@app.route("/hbnb")
+def hbnb():
+    return "HBNB"
+
+@app.route("/c/<text>")
+def text_display(text):
+    """
+    Display "C " followed by the value of the text variable.
+    Replace underscores with spaces
+    """
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+@app.route("/python/<text>=is_cool")
+def display_text(text):
+    text_with_spaces = text_wth_spaces.replace('_', ' ')
+    return "Python {}".format(text_with_spaces)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
