51a256015f88446ca5b3b8b9517e7bdca4b40c5c
chykB <malikchika86@gmail.com>
2023-08-28 18:47:31 -0700

    a script that defines  a dynamic route that display a number.

--- python-web_framework/4-number_route.py
+++ python-web_framework/4-number_route.py
@@ -35,7 +35,7 @@ def display_text(text="is cool"):
 
 @app.route("/number/<n>")
 def display_number(n):
-    if type(n)==int:
+    if isinstance(n, int):
         return "{} is a number".format(n)
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
