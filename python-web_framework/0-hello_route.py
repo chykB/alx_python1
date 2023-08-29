ac511256c3c6972998a8471e43c8160b65820f7f
chykB <malikchika86@gmail.com>
2023-08-28 17:43:09 -0700

    updated

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -12,7 +12,7 @@ def index():
     The application has a single route, `/`,
     which calls the `index()` function
     """
-    return "<p>Hello HBNB!<p>"
+    return "Hello HBNB!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
