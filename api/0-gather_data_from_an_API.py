0ad2c048c0106fc0afe17cd55b16b6e2e31cef2b
ChykB <malikchika86@gmail.com>
2023-10-01 00:01:56 +0100

    updated

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -27,7 +27,7 @@ for task in todo_data:
     total_number_of_tasks += 1
 
 
-print(f"Employee {employee_name} is done with tasks ({number_of_done_tasks}/{total_number_of_tasks}):")
+print(f"Employee {employee_name} is done with tasks ({number_of_done_tasks}/{total_number_of_tasks})")
 
 for task in todo_data:
     print(f"\t{task['title']}")
